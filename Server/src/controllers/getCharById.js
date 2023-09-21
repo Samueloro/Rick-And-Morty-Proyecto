8002cdc90b99afd0be8df06d9caea2dd9e7ec15e
Samuel Rodríguez <samuelmateo986@gmail.com>
2023-09-21 10:49:18 -0500

    PromesasController

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+
+const getCharById = (res, id) => {
+    axios(` https://rickandmortyapi.com/api/character/${id}`)
+        .then(({ data }) => {
+            const character = {
+                id: data.id,
+                name: data.name,
+                gender: data.gender,
+                species: data.species,
+                origin: data.origin,
+                image: data.image,
+                status: data.status,
+            }
+            return res
+                .writeHead(200, { 'Content-type': 'aplication/json' })
+                .end(JSON.stringify(character))
+        })
+        .catch(error => {
+            return res
+                .writeHead(500, { 'Content-type': 'text/plain' })
+                .end(error.message)
+        })
+}
+
+
+module.exports = getCharById;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,14 @@
 const http = require('http')
-const characters = require('./utils/data.js')
+const getCharById = require('./controllers/getCharById.js')
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    if (req.url.includes('/rickandmorty/character')) {
+    if (req.url.includes('/rickandmorty/character')){
         const partId = req.url.split('/')
         const idString = partId[partId.length - 1]
         const id = Number(idString)
-        
-        const searchCharacter = characters.find(character => character.id === id)
-        if (searchCharacter) {
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            res.end(JSON.stringify(searchCharacter))
-        } else {
-            res.writeHead(404, { 'Content-Type': 'application/json' })
-            res.end('Personaje no encontrado')
-        }
+
+        getCharById(res, id)
     }
+
 }).listen(3001, 'localhost');
